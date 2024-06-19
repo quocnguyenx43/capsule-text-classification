2bc46c0a9d34c4fceddedef41cec70d7fb10c25f
quocnguyen <quocnguyenx43@gmail.com>
2024-06-19 13:46:08 +0700

    add

--- run.py
+++ run.py
@@ -220,7 +220,7 @@ class CapsNet(nn.Module):
         
     def forward(self, ids, attn_mask):
         print('he')
-        x = self.phobert(ids, attn_mask).last_hidden_state.squeeze(-1)
+        x = self.phobert(ids, attn_mask).last_hidden_state.unsqueeze(-1).permute(0, 2, 1, 3)
         print(x.shape)
         output = self.digit_capsules(self.primary_capsules(self.conv_layer(x)))
         reconstructions, masked = self.decoder(output, x)
